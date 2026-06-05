1314f50844a7ebb2df2f8a9378a7877f705701a4
Prashant123-kumar <162018717+Prashant123-kumar@users.noreply.github.com>
2026-06-05 22:03:09 +0530

    Increase timeout for GitHub API requests

--- Frontend/src/api/githubApi.js
+++ Frontend/src/api/githubApi.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const githubApi = axios.create({
   baseURL: "https://github-repo-explorer-api-8sgt.onrender.com/api/github",
-  timeout: 60000,
+  timeout: 100000,
   headers: {
     "Content-Type": "application/json",
   }
