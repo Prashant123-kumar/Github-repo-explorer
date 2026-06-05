02b30b352dfe2f58445868cfedfb0979ae61bf86
Prashant kumar <kumarprashantshivam456@gmail.com>
2026-06-05 16:29:04 +0530

    Updated backend API URL

--- Frontend/src/api/githubApi.js
+++ Frontend/src/api/githubApi.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const githubApi = axios.create({
   // Matches your backend @RequestMapping base path
-  baseURL: "http://localhost:8080/api/github", 
+  baseURL: "https://github-repo-explorer-api-8sgt.onrender.com/api/github" ,
   timeout: 10000,
   headers: {
     "Content-Type": "application/json",
